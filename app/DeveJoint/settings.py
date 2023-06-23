fb4838d22ee4516c210d1b83ce3a61618e7e5388
V-U-Simon <V-U-Simon@yandex.ru>
2023-06-23 15:52:44 +0300

    set up environ variable
    For comfortable local developing.
    
    Исправлена неточность в конекте к БД, т.к. в случае локального запуска (не черезе докер) постгря висит на локальном хосте.

--- app/DeveJoint/settings.py
+++ app/DeveJoint/settings.py
@@ -15,19 +15,17 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY", 'secret-123')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get("DEBUG"))
+DEBUG = bool(os.environ.get("DEBUG", True))
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", '*').split(" ")
 INTERNAL_IPS = ["127.0.0.1"]
-
 
 # Application definition
 
@@ -41,7 +39,6 @@ INSTALLED_APPS = [
     ################
     "debug_toolbar",
     ################
-
 ]
 
 MIDDLEWARE = [
@@ -75,18 +72,17 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "DeveJoint.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("POSTGRES_DB"),
-        "USER": os.getenv("POSTGRES_USER"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-        "HOST": os.getenv("POSTGRES_HOST"),
-        "PORT": os.getenv("EXT_PG_PORT"),
+        "NAME": os.getenv("POSTGRES_DB") or 'deve_joint',
+        "USER": os.getenv("POSTGRES_USER") or 'postgres',
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD") or 'passw0rd',
+        "HOST": os.getenv("POSTGRES_HOST") or 'localhost',
+        "PORT": os.getenv("EXT_PG_PORT") or '16432',
     }
 }
 
@@ -97,11 +93,16 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",},
-    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
-    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
@@ -113,7 +114,6 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/

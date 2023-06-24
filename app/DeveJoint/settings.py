62ee04a84da5654288756707e7e90849cedd373f
V-U-Simon <V-U-Simon@yandex.ru>
2023-06-24 11:45:04 +0300

    main: settings - media & static

--- app/DeveJoint/settings.py
+++ app/DeveJoint/settings.py
@@ -119,12 +119,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+MEDIA_URL = "media/"
+
 STATICFILES_FINDERS = (
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 )
 
 STATIC_ROOT = "/static"
+MEDIA_ROOT = "/medial"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

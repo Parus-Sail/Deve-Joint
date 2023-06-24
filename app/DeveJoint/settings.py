c96f205efbbeb062d93da59ea73dfa938bd549b2
V-U-Simon <V-U-Simon@yandex.ru>
2023-06-24 14:18:03 +0300

    main: settings - fix

--- app/DeveJoint/settings.py
+++ app/DeveJoint/settings.py
@@ -127,7 +127,7 @@ STATICFILES_FINDERS = (
 )
 
 STATIC_ROOT = "/static"
-MEDIA_ROOT = "/medial"
+MEDIA_ROOT = "/media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

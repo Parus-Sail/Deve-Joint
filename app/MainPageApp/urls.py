807062895a71dc9ca6eb6ff7492d673d5e85ded8
Sergey <kuchko.02@gmail.com>
2023-06-29 14:37:27 +0300

    Add news urls

--- app/MainPageApp/urls.py
+++ app/MainPageApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.views.generic import RedirectView
 
 from .apps import MainpageappConfig
-from .views import AboutPageView, ContactsPageView, JobsDetailView, JobsListingView, MainPageView
+from .views import *
 
 app_name = MainpageappConfig.name
 
@@ -13,4 +13,9 @@ urlpatterns = [
     path("contacts/", ContactsPageView.as_view(), name="contacts"),
     path("job-listing/", JobsListingView.as_view(), name="job-listing"),
     path("job-details/", JobsDetailView.as_view(), name="job-details"),
+    path('news/', NewsListView.as_view(), name='news'),
+    path('news/<int:pk>/detail', NewsDetailView.as_view(), name='news_detail'),
+    path('news/create/', NewsCreateView.as_view(), name='news_create'),
+    path('news/<int:pk>/update/', NewsUpdateView.as_view(), name='news_update'),
+    path('news/<int:pk>/delete', NewsDeleteView.as_view(), name='news_delete'),
 ]

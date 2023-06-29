717a3b81e008b90987df56dc050895f0a353ac30
V-U-Simon <V-U-Simon@yandex.ru>
2023-06-29 22:05:30 +0300

    projects_app: service

---
+++ app/projects_app/service.py
@@ -0,0 +1,49 @@
+from django.db import transaction
+from projects_app.models import Membership, Project
+from roles_app.models import Role
+
+from .models import Membership, Project
+
+
+@transaction.atomic
+def create_project(title: str, description: str, owner):
+
+    project = Project.objects.create(
+        title=title,
+        description=description,
+        owner=owner,
+    )
+
+    admin_role, created = Role.objects.get_or_create(name='admin')
+
+    founder = Membership.objects.create(
+        project=project,
+        user=owner,
+        role=admin_role,
+    )
+
+    return project
+
+
+@transaction.atomic
+def delete_project(project):
+    project.delete()
+
+
+@transaction.atomic
+def update_project(project, **kwargs):
+    for key, value in kwargs.items():
+        setattr(project, key, value)
+    project.save()
+
+
+@transaction.atomic
+def include_member(project, user, role):
+    membership = Membership.objects.create(project=project, user=user, role=role)
+    return membership
+
+
+@transaction.atomic
+def exclude_member(project, user):
+    membership = Membership.objects.get(project=project, user=user)
+    membership.delete()

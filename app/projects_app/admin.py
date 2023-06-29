64e5d70c8a6ac4230e7cb98fd28568a3fed2a688
V-U-Simon <V-U-Simon@yandex.ru>
2023-06-29 22:04:16 +0300

    projects_app: admin

--- app/projects_app/admin.py
+++ app/projects_app/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Membership, Project
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description')
+    # list_filter = ('role',)
+    # search_fields = ('title',)
+    # search_fields = ('user__username', 'project__name', 'role__name')
+
+
+@admin.register(Membership)
+class MembershipAdmin(admin.ModelAdmin):
+    ...
+    # list_display = ('user', 'project', 'role')
+    # Customize the displayed fields as needed

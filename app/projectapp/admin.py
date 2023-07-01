0ddbb48bb747a02cc3eaeaf11137cfd5d218464e
V-U-Simon <V-U-Simon@yandex.ru>
2023-07-01 18:09:53 +0300

    projectapp & roleapp: admin

--- app/projectapp/admin.py
+++ app/projectapp/admin.py
@@ -1,14 +1,22 @@
 from django.contrib import admin
+from roleapp.models import Role
 
 from .models import Membership, Project
+
+
+class RoleInline(admin.TabularInline):
+    model = Role
+    extra = 0
 
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('title', 'description')
+
     # list_filter = ('role',)
     # search_fields = ('title',)
     # search_fields = ('user__username', 'project__name', 'role__name')
+    inlines = [RoleInline]
 
 
 @admin.register(Membership)

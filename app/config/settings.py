c0e0cc3fdc012968425a8126a0c738140cb9c5b2
V-U-Simon <V-U-Simon@yandex.ru>
2023-07-03 00:55:53 +0300

    auth_app: auth redirect url

--- app/config/settings.py
+++ app/config/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+from audioop import reverse
 import os
 from pathlib import Path
 
@@ -171,3 +172,5 @@ else:
 
 # Custom user model
 AUTH_USER_MODEL = "auth_app.BaseOpenSailUser"
+
+LOGIN_URL = 'auth_app:login'
